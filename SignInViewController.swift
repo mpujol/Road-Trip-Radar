c6a8cfef4ca54f97ae816daae45fea4be03a67f5
Michael Andrew Pujol <mike.pujol88@gmail.com>
2016-08-24 23:51:53 -0700

    Minor UI fixes
    Fixed the profile header in Splash Page

--- SignInViewController.swift
+++ SignInViewController.swift
@@ -186,13 +186,6 @@ class SignInViewController: UIViewController {
             
         })
         
-//        //Add the newly created user to the database
-//        let newUser = User(user: (FIRAuth.auth()?.currentUser!)!)
-//        let newUserRef = ref.child("\(newUser.uid)")
-//        newUserRef.setValue(newUser.toAnyObject())
-//        
-//        self.signedIn(FIRAuth.auth()?.currentUser)
-        
     }
     
     func setDisplayName(user: FIRUser!) {
