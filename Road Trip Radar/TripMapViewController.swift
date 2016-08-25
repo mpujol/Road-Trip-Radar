69f8dc6dc5b0f1ab7aa18b993c2267bf64ea7341
Michael Andrew Pujol <mike.pujol88@gmail.com>
2016-08-25 16:32:19 -0700

    Updated the TripMap VC to show the users location

--- Road Trip Radar/TripMapViewController.swift
+++ Road Trip Radar/TripMapViewController.swift
@@ -10,18 +10,30 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class TripMapViewController: UIViewController, MKMapViewDelegate {
+class TripMapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
-    
-    
-    
     
     var currentTrip: Trip!
     
     
+    
+    lazy var locationManager: CLLocationManager = {
+        var _locationManager = CLLocationManager()
+        _locationManager.delegate = self
+        _locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        _locationManager.activityType  = .AutomotiveNavigation
+        
+        // Movement threshold for new events
+        _locationManager.distanceFilter = 10.0
+        return _locationManager
+    }()
+    
+    
     private var spanLatitudeDelta = 0.2
     private var spanLongitudeDelta = 0.2
+    
+    // MARK: View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,11 +47,24 @@ class TripMapViewController: UIViewController, MKMapViewDelegate {
         
         showDestinationRegion()
         createDestinationAnnotation()
+        startUpdatingLocation()
         
     }
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Perform any additional setup here
+        locationManager.requestAlwaysAuthorization()
+        print(CLLocationManager.authorizationStatus().rawValue)
+        
+        
+    }
+    
+    
     // MARK: Helper Functions
     
+    // Final Destination
     func showDestinationRegion() {
         
         let currentlocationCenter = CLLocationCoordinate2D(latitude: (currentTrip.latitude as NSString).doubleValue, longitude: (currentTrip.longitude as NSString).doubleValue)
@@ -58,6 +83,14 @@ class TripMapViewController: UIViewController, MKMapViewDelegate {
         self.mapView.addAnnotation(destinationLocattioinAnnotation)
     }
     
+    // Location Tracking
+    func startUpdatingLocation() {
+        
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
+        
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -65,7 +98,11 @@ class TripMapViewController: UIViewController, MKMapViewDelegate {
     }
     
     
+    // MARK: CLLocationManagerDelegate
     
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        <#code#>
+    }
 
     /*
     // MARK: - Navigation
